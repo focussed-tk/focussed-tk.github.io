698e2ffbf9801e576f1705edbe8acd8d1ff60160
(っ◔◡◔)っ ♥ nick ♥ <118109836+focussed-tk@users.noreply.github.com>
2022-11-13 06:16:24 -0600

    Add files via upload

---
+++ js/scale.js
@@ -0,0 +1,6 @@
+if(screen.width >=2500)
+	document.write("<style> body {zoom:145.2%} </style>")
+if(screen.width <=1700)
+	document.write("<style> body {zoom:90.75%} </style>")
+if(screen.width <=1400)
+	document.write("<style> body {zoom:61.875%} </style>")
